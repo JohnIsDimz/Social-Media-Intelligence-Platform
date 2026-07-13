c3cfbac7d07eef0ad810b3bae90e88aac5135d13
JohnIsDimz <johnisdimz@gmail.com>
2026-07-14 00:24:23 +0700

    Add files via upload

--- server.ts
+++ server.ts
@@ -40,7 +40,7 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
   httpOptions: {
     headers: {
-      'User-Agent': 'aistudio-build',
+      'User-Agent': 'mediatrend-build',
     }
   }
 });
